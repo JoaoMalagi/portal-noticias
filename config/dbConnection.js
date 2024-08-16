ebcb674b11d8f7586c2174ad7210f1b926474fe2
João Pedro <joaopedromalagi@hotmail.com>
2024-08-16 15:45:03 -0300

    Criacao formulario e salvando no banco

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,11 +1,17 @@
 var mysql = require('mysql');
- 
-module.exports = function() {
 
+//criando uma variavel que recebe uma funcão, para evitar criar conexoes com o banco sem necessidade
+
+var connMySQL = function(){
+    console.log("Conexao BD")
    return mysql.createConnection({
         host: 'localhost',
         user:'root',
         password: 'admin',
         database: 'portal_noticias'
     });
+}
+module.exports = function() {
+    console.log("conexao autoload")
+    return connMySQL;
 } 
--- config/server.js
+++ config/server.js
@@ -1,6 +1,21 @@
 var express = require('express');
+var consign = require('consign');
+var bodyParser = require('body-parser');
+
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+//detecta todas as rotas e inclue elas no app, evitando fazer requires no proprio app.js
+//Ele executa tudo que esta no exports
+consign()
+    .include('app/routes')
+    //Dessa maneira nós indicamos o modulo especifico para ser executado pelo consign, assim ele so executa aquele arquivo da pasta config
+    .then('config/dbConnection.js')
+    .then('app/models')
+    .into(app);
+
 
 module.exports = app;
