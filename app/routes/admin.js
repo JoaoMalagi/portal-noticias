c2649a13591401df896592c13a68a3782d32c927
João Pedro <joaopedromalagi@hotmail.com>
2024-08-19 15:00:50 -0300

    Criando classes e implementando validações no post

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,14 +1,30 @@
+const {body, validationResult} = require('express-validator')
+
 module.exports = function(app){
     app.get('/formulario_inclusao_noticia', function(req, res){
-        res.render("admin/form_add_noticia");   
+        res.render("admin/form_add_noticia", {errors : {}, noticia : {}});   
     })
-    app.post('/noticias/salvar', function(req, res){
+    app.post('/noticias/salvar',[body("titulo").notEmpty().withMessage("TItulo nao pode ser vazio"),
+        body('noticia').notEmpty().withMessage("Noticia não pode ser vazia")
+    ], function(req, res){
+          
         var noticia = req.body;
+        const errors = validationResult(req).formatWith(({msg}) => msg);
+        
+        console.log(errors.array())
+        if (!errors.isEmpty()){
+            
+            res.render("admin/form_add_noticia", {errors : errors.array(), noticia: noticia});
+            console.log(errors.array())
+            return;
+        }
         var connection = app.config.dbConnection();
-        var noticiasModel = app.app.models.noticiasModel;
+        var NoticiasDAO = new app.app.models.NoticiasDAO(connection);
 
-        noticiasModel.salvarNoticia(noticia, connection, function(error, result){
+        NoticiasDAO.salvarNoticia(noticia, function(error, result){
+            console.log(noticia)
             res.redirect("/noticias");
         });      
+
     })
 }
