de7d058c4e17874128863bc0d6daeefa79dcf8db
João Pedro <joaopedromalagi@hotmail.com>
2024-08-21 15:51:10 -0300

    Inicio da edição de noticias

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -1,8 +1,25 @@
 const {validationResult} = require('express-validator')
 
 module.exports.formulario_inclusao_noticia = function(app, req, res){
-    res.render("admin/form_add_noticia", {errors : {}, noticia : {}});
-}
+    
+    //var noticiaid = Object.keys(req.query).length
+    //console.log(noticiaid)
+    //console.log(req)
+    if (req._parsedOriginalUrl.query != null)  {
+        console.log("sem ID");
+        var connection = app.config.dbConnection();
+        var NoticiasDAO = new app.app.models.NoticiasDAO(connection);
+        var id_noticia = req.query;
+        NoticiasDAO.getNoticia(id_noticia, function(error, result,){
+            res.render("admin/form_add_noticia", {errors : {}, noticia : result});
+            console.log(result)
+        });  
+    }   
+    else{
+        res.render("admin/form_add_noticia", {errors : {}, noticia : [{}]});
+    }
+} 
+           
 
 module.exports.noticias_salvar = function(app, req, res){
     
@@ -25,3 +42,4 @@ module.exports.noticias_salvar = function(app, req, res){
         });      
 
 }
+ 
